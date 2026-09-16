54952688503950094c704c69b17078721cebc760
Михаил Собянин <sobianinmisha2000@gmail.com>
2026-09-17 03:04:15 +0500

    Task3

---
+++ Tyuiu.SobyaninMI.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.SobyaninMI.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tyuiu.SobyaninMI.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,3 @@
+﻿using Tyuiu.SobyaninMI.Sprint0.Task3.V0.Lib;
+Console.WriteLine(DataService.Sum(7, 7));
+Console.ReadLine();
