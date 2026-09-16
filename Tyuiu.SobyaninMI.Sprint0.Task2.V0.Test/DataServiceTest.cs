45fa4971beceb6d1bc53d86162d87e3aae568a4a
Михаил Собянин <sobianinmisha2000@gmail.com>
2026-09-17 02:34:22 +0500

    Task2

---
+++ Tyuiu.SobyaninMI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SobyaninMI.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SobyaninMI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Михаил";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Михаил", res);
+        }
+    }
+}
---
+++ Tyuiu.SobyaninMI.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.SobyaninMI.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Михаил"));
+Console.ReadKey();
