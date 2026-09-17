1e831c5a0812ffd9a4fcec8973d2574d307ad544
Михаил Собянин <sobianinmisha2000@gmail.com>
2026-09-17 10:25:42 +0500

    Task6

---
+++ Tyuiu.SobyaninMI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.SobyaninMI.Sprint0.Task6.V0.Lib;
+
+int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Сумма эллементов массива = " + DataService.AdditionArray(numsArray));
+Console.WriteLine("Разность эллементов массива = " + DataService.SubtractionArray(numsArray));
+Console.WriteLine("Произведение эллементов массива = " + DataService.MultiplicationArray(numsArray));
+
+Console.ReadKey();
